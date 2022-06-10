bd115c43a35b21f75e6714294ea7fae7bb047cad
Jose Reginaldo <josejunior2@iesb.br>
2022-06-09 22:00:52 -0300

    Adicionando ao repositorio

---
+++ app/src/main/java/com/example/atividade04/ListaActivity.kt
@@ -0,0 +1,21 @@
+package com.example.atividade04
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class ListaActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_lista)
+        setSupportActionBar(findViewById(R.id.tbrLista))
+        setTitle("@Contatinhos")
+
+        val botao = findViewById<FloatingActionButton>(R.id.fabAdicionar)
+        botao.setOnClickListener {
+            val intencao = Intent(applicationContext, CadastroActivity::class.java)
+            startActivity(intencao)
+        }
+    }
+}
